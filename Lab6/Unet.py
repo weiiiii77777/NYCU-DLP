3ecfcffecf0d295a21a18a969945465c3b93ce02
weiiiii77777 <80786995+weiiiii77777@users.noreply.github.com>
2025-02-20 20:04:07 +0800

    Add files via upload

---
+++ Lab6/Unet.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+from diffusers import UNet2DModel
+from diffusers.models.embeddings import GaussianFourierProjection, TimestepEmbedding, Timesteps
+
+class Unet(nn.Module):
+    def __init__(self, labels_num = 24, embedding_label_size=4) -> None:
+        super().__init__()
+        self.label_embedding = nn.Embedding(labels_num, embedding_label_size)
+        self.model = UNet2DModel(
+            sample_size = 64,
+            # in_channels = 3 + labels_num * embedding_label_size,
+            in_channels = 3 + labels_num,
+            out_channels = 3,
+            time_embedding_type = "positional",
+            layers_per_block = 2,
+            block_out_channels = (128, 128, 256, 256, 512, 512),  # number of output channels for each UNet block
+            down_block_types = (
+                "DownBlock2D",  # a regular ResNet downsampling block
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                "DownBlock2D",
+            ),
+            up_block_types = (
+                "UpBlock2D",  # a regular ResNet upsampling block
+                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+            ),
+        )
+    def forward(self, x, t, label):
+        bs, c, w, h = x.shape
+        embeded_label = label.view(bs, label.shape[1], 1, 1).expand(bs, label.shape[1], w, h)
+        unet_input = torch.cat((x, embeded_label), 1)
+        unet_output = self.model(unet_input, t).sample
+        return unet_output
---
+++ Lab6/dataloader.py
@@ -0,0 +1,67 @@
+import os, json
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+import numpy as np
+
+
+class iclevrDataset(Dataset):
+    def __init__(self, root='../dataset/iclevr', _transforms=None, mode="train",):
+        super().__init__()
+        assert mode in ['train', 'test', 'new_test', 'denoise'], "There is no such mode !!!"
+        # initialize img_paths
+        if mode == 'train':
+            with open('../dataset/train.json', 'r') as json_file:
+                self.json_data = json.load(json_file)
+            self.img_paths = list(self.json_data.keys())
+            self.labels = list(self.json_data.values())
+        elif mode == 'test':
+            with open('../dataset/test.json', 'r') as json_file:
+                self.json_data = json.load(json_file)
+            self.labels = self.json_data
+        elif mode == 'new_test':
+            with open('../dataset/new_test.json', 'r') as json_file:
+                self.json_data = json.load(json_file)
+            self.labels = self.json_data
+        elif mode == 'denoise':
+            with open('../dataset/denoise.json', 'r') as json_file:
+                self.json_data = json.load(json_file)
+            self.labels = self.json_data
+        # initailize one-hot labels
+        self.labels_one_hot = []
+        with open('../dataset/objects.json', 'r') as json_file:
+            self.objects_dict = json.load(json_file)
+        for label in self.labels:
+            label_one_hot = [0] * len(self.objects_dict)
+            for text in label:
+                label_one_hot[self.objects_dict[text]] = 1
+            self.labels_one_hot.append(label_one_hot)
+        # initialize others
+        self.root = root   
+        self.mode = mode
+            
+    def __len__(self):
+        return len(self.labels)      
+    
+    def __getitem__(self, index):
+        if self.mode == 'train':
+            transform_img = transforms.Compose([
+                transforms.Resize((64, 64)),
+                transforms.ToTensor(),
+                 transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
+            img_path = os.path.join(self.root, self.img_paths[index])
+            img = Image.open(img_path).convert("RGB")
+            img = transform_img(img)
+            label_one_hot = torch.tensor(np.array(self.labels_one_hot[index]))
+            return img, label_one_hot
+        elif self.mode == 'test':
+            label_one_hot = torch.tensor(np.array(self.labels_one_hot[index]))
+            return label_one_hot
+        elif self.mode == 'new_test':
+            label_one_hot = torch.tensor(np.array(self.labels_one_hot[index]))
+            return label_one_hot
+        elif self.mode == 'denoise':
+            label_one_hot = torch.tensor(np.array(self.labels_one_hot[index]))
+            return label_one_hot
